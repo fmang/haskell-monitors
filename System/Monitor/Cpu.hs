0e08bcfa834a5b7fab33a2f4c6a35d6b0d40f633
Frédéric Mangano <fred.mangano@gmail.com>
2012-03-05 19:46:37 +0100

    cpu monitor

---
+++ System/Monitor/Cpu.hs
@@ -0,0 +1,23 @@
+module System.Monitor.Cpu (Jiffy, Status (..), getStatus) where
+
+import Data.Maybe (mapMaybe)
+import qualified Data.ByteString.Lazy.Char8 as B
+
+type Jiffy = Int
+
+data Status = Status
+    { user   :: Jiffy
+    , nice   :: Jiffy
+    , system :: Jiffy
+    , idle   :: Jiffy
+    , ioWait :: Jiffy
+    } deriving (Eq, Show)
+
+parse :: B.ByteString -> Status
+parse = f . mapMaybe (fmap fst . B.readInt) . B.words . B.takeWhile (/= '\n')
+  where f (_:us:n:sys:idl:io:_) = Status us n sys idl io
+        f _ = error "invalid /proc/stat"
+
+-- | Read @\/proc\/stat@.
+getStatus :: IO Status
+getStatus = parse `fmap` B.readFile "/proc/stat"
