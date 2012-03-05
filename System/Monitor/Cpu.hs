eab0ec4fde2926ec2244f323db1300b189228407
Frédéric Mangano <fred.mangano@gmail.com>
2012-03-05 20:17:30 +0100

    cpu: operations on Status

--- System/Monitor/Cpu.hs
+++ System/Monitor/Cpu.hs
@@ -1,4 +1,11 @@
-module System.Monitor.Cpu (Jiffy, Status (..), getStatus) where
+module System.Monitor.Cpu
+    ( -- * Types
+      Jiffy, Status (..)
+    , -- * Operations
+      diff, active, total
+    , -- * IO
+      getStatus
+    ) where
 
 import Data.Maybe (mapMaybe)
 import qualified Data.ByteString.Lazy.Char8 as B
@@ -12,6 +19,16 @@ data Status = Status
     , idle   :: Jiffy
     , ioWait :: Jiffy
     } deriving (Eq, Show)
+
+diff :: Status -> Status -> Status
+diff (Status u' n' s' i' w') (Status u n s i w) =
+    Status (u'-u) (n'-n) (s'-s) (i'-i) (w'-w)
+
+active :: Status -> Jiffy
+active s = user s + nice s + system s
+
+total :: Status -> Jiffy
+total (Status u n s i w) = u + n + s + i + w
 
 parse :: B.ByteString -> Status
 parse = f . mapMaybe (fmap fst . B.readInt) . B.words . B.takeWhile (/= '\n')
