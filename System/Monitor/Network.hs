8428ec541c433c788dab37f3f82a2a2a8d0a81bc
Frédéric Mangano <fred.mangano@gmail.com>
2012-03-07 14:55:03 +0100

    network: Integer -> Int
    64-bit Ints are large enough

--- System/Monitor/Network.hs
+++ System/Monitor/Network.hs
@@ -6,16 +6,16 @@ import qualified Data.ByteString.Lazy.Char8 as B
 
 -- | Network interface.
 data Interface = Interface
-    { interface :: String  -- ^ Interface name.
-    , rx        :: Integer -- ^ Bytes received.
-    , tx        :: Integer -- ^ Bytes transmitted.
+    { interface :: String -- ^ Interface name.
+    , rx        :: Int    -- ^ Bytes received.
+    , tx        :: Int    -- ^ Bytes transmitted.
     } deriving (Eq, Show)
 
 parseLine :: B.ByteString -> Maybe Interface
 parseLine str =
     let (ifname, rest) = B.break (== ':') (B.dropWhile (== ' ') str)
         ws = if B.null rest then [] else B.words (B.tail rest)
-        nums = mapMaybe (fmap fst . B.readInteger) ws
+        nums = mapMaybe (fmap fst . B.readInt) ws
     in case nums of
         (r:_:_:_:_:_:_:_:t:_) -> Just $ Interface (B.unpack ifname) r t
         _ -> Nothing
